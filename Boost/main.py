6580147426fc944190bca39182fdc99bd0c998ad
Gustavo Gutierrez <54781049+Gustavo2022003@users.noreply.github.com>
2023-08-06 21:00:38 -0300

    Update main.py

--- Boost/main.py
+++ Boost/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 __dirname = os.path.dirname(__file__)
 files_counter = 0
@@ -10,13 +11,22 @@ def title(txt):
     print(f'  \033[92m{txt}\033[m')
     print('=' * (len(txt) + 4))
 
-title("Bem vindo ao PC BOOSTER!")
-print("1 - Apagar arquivos temporários")
-print("2 - Mostrar diratório atual")
-print("3 - Sair\n")
-user_option = int(input("Digite a opção: "))
+def menu():
+    title("Bem vindo ao PC BOOSTER!")
+    print("1 - Apagar arquivos temporários")
+    print("2 - Mostrar diratório atual")
+    print("3 - Mostrar todos os softwares instalados")
+    print("4 - Desinstalar software")
+    print("5 - Mostrar informações do sistema")
+    print("6 - Sair\n")
+    user_option = int(input("Digite a opção: "))
+    if user_option == 6:
+        exit()
+    return user_option
 
-while user_option != 3:
+user_option = menu()
+
+while user_option != 6:
 
     files_counter = 0
 
@@ -81,26 +91,34 @@ while user_option != 3:
         
         # Voltar
         if user_option_arquivos == 4:
-            title("Bem vindo ao PC BOOSTER!")
-            print("1 - Apagar arquivos temporários")
-            print("2 - Mostrar diratório atual")
-            print("3 - Sair\n")
-            user_option = int(input("Digite a opção: "))
+            menu()
 
     # Mostrar diretório atual
     if user_option == 2:
         print("\nDiretório atual: ")
         print(f'\n\033[94m{__dirname}\033[m')
-        title("Bem vindo ao PC BOOSTER!")
-        print("1 - Apagar arquivos temporários")
-        print("2 - Mostrar diratório atual")
-        print("3 - Sair\n")
-        user_option = int(input("Digite a opção: "))
+        menu()
+
+    # Mostrar todos os softwares instalados
+    if user_option == 3:
+        print("\n\033[92mSoftwares instalados:\033[m")
+        os.system("wmic product get name,version")
+        menu()
+
+    # Desinstalar software
+    if user_option == 4:
+        software_name = input("\nDigite o nome do software: ")
+        os.system(f'wmic product where "name like \'%{software_name}%\'" call uninstall')
+        print(f'\n\033[92m{software_name} desinstalado com sucesso!\033[m')
+        menu()
+
+    # Mostrar informações do sistema
+    if user_option == 5:
+        print("\n\033[92mInformações do sistema:\033[m")
+        os.system("systeminfo")
+        menu()
 
     # Sair
-    if user_option == 3:
+    if user_option == 6:
         print("\033[93mSaindo...\033[m")
-        break
-else:
-    print("\033[93mSaindo...\033[m")
-    exit()
+        exit()
