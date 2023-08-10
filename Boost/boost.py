943096658420479a307d50641e298a6790b1d66b
Gustavo Gutierrez <54781049+Gustavo2022003@users.noreply.github.com>
2023-08-10 00:25:01 -0300

    Update boost.py

--- Boost/boost.py
+++ Boost/boost.py
@@ -24,9 +24,11 @@ def main():
     elif user_input == 2:
         print("\nDiretório atual: ")
         print(f'\n\033[94m{__dirname}\033[m')
+        input("\n\033[35mAperte ENTER para continuar...\033[m")
     elif user_input == 3:
         print("\n\033[92mInformações do sistema:\033[m")
         os.system("systeminfo")
+        input("\n\033[35mAperte ENTER para continuar...\033[m")
     elif user_input == 4:
         desligar_suspender()
     elif user_input == 5:
